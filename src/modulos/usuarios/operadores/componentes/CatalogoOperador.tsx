cbdcf55b9b7fea084d94e6eeec56a59f05c5d401
SimplementeDiego <diegodellepianefurrer@gmail.com>
2026-09-18 20:18:03 -0300

    feat: Creo algunos archivos iniciales. Creo función de obtener perfil del operador de la base de datos. Inicio estructura de la vista.

---
+++ src/modulos/usuarios/operadores/componentes/CatalogoOperador.tsx
@@ -0,0 +1,9 @@
+type CatalogoOperadorProps = {
+  publicaciones: unknown[];
+};
+
+export default function CatalogoOperador({ publicaciones }: CatalogoOperadorProps) {
+  void publicaciones;
+
+  return null;
+}
