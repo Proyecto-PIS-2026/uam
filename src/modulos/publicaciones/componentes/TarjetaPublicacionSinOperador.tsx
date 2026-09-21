b044d750a199fad90cce2177049a5ca232fc1de6
martinanido22 <martin.anido.22@gmail.com>
2026-09-21 20:13:21 -0300

    refactor: Diseño tarjeta publicacion

--- src/modulos/publicaciones/componentes/TarjetaPublicacionSinOperador.tsx
+++ src/modulos/publicaciones/componentes/TarjetaPublicacionSinOperador.tsx
@@ -20,9 +20,16 @@ export function PublicacionSinOperador ({ producto, onPublicacionClick }: Public
     const contenido =   (
         <div className={styles.tarjeta} onClick ={() => onPublicacionClick(producto.id)}>
             <div className={styles.imagen}>
-                <img 
-                    src={producto.foto !== null ? producto.foto : "/imagenes/producto-sin-foto.png"}
-                />
+                {producto.foto !== null ? (
+                    <img 
+                        src={producto.foto} 
+                        alt={producto.especie} 
+                    />
+                ) : (
+                    <span className={styles.sin_foto}>
+                        IMG
+                    </span>
+                )}
             </div>
             <div className={styles.contenido}> 
                 <div className={styles.nombre}>
@@ -30,20 +37,24 @@ export function PublicacionSinOperador ({ producto, onPublicacionClick }: Public
                         {" "}
                         {producto.variedad !== "-" && <>{producto.variedad}</>}
                 </div>
-                <div className={styles.etiquetas}>
-                    <span className={styles.etiqueta}>
-                        {producto.presentacion}
-                    </span>
-                    <span className={styles.etiqueta}>
-                        {producto.calibre}
-                    </span>
-                    <span className={styles.etiqueta}>
-                        {producto.categoria}
-                    </span>
-                </div>
-                <div className={styles.precio}>
-                    {producto.precio !== null ? <>$ {producto.precio}</> : "Sin precio"}
-                </div>
+                <div className={styles.bloque_detalle}>
+                    <div>
+                        <div className={styles.presentacion}>
+                            {producto.presentacion}
+                        </div>
+                        <div className={styles.etiquetas}>
+                            <span className={styles.etiqueta}>
+                                {producto.calibre}
+                            </span>
+                            <span className={styles.etiqueta}>
+                                {producto.categoria}
+                            </span>
+                        </div>   
+                    </div>
+                    <div className={styles.precio}>
+                        {producto.precio !== null ? <>$ {producto.precio}</> : "Sin precio"}
+                    </div>
+                </div>         
             </div>
         </div>
     );
