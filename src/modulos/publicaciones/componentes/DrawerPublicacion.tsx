1ca7af1be763860504671f79802e9e551b32571a
Santiago Santana Deniz <santi.santnadenizzzz@gmail.com>
2026-09-23 14:49:23 -0300

    feat: continuacion del drawer

--- src/modulos/publicaciones/componentes/DrawerPublicacion.tsx
+++ src/modulos/publicaciones/componentes/DrawerPublicacion.tsx
@@ -3,6 +3,8 @@
 import styles from "./DrawerPublicacion.module.css";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import Image from "next/image";
+import { Button } from "@mui/material";
 
 export type DetallePublicacion = {
     id: number; 
@@ -30,62 +32,60 @@ interface DrawerPublicacionProps {
 
 } 
 
-export function DrawerPublicacion({ producto, open, onOpenChange}: DrawerPublicacionProps) {
+export function DrawerPublicacion({producto, open, onOpenChange}: DrawerPublicacionProps) {
     const contenido = (
         <SwipeableDrawer 
             anchor="bottom"
             open={open}
             onClose={() => onOpenChange(false)}
             onOpen={() => onOpenChange(true)}
-            slotProps={{paper: { className: styles.drawer }}}
+            slotProps={{paper:{className: styles.drawer}}}
         >
             <div className={styles.indicador}/>
-
-            <div className={styles.contenido}>
-                <div className={styles.tarjeta}>
-                    <div className={styles.bloque}> 
-                        <div className={styles.imagen}>
-                            {producto.foto !== null ? (
-                                <img src={producto.foto}/>
-                            ) : (
-                                <span>IMG</span>
-                            )
+            <div className={styles.tarjeta}>
+                <div  className={styles.bloque_superior}>
+                    <div className={styles.imagen}> 
+                        {producto.foto ? <Image src={producto.foto} alt={producto.especie}/>
+                        : <span className={styles.sin_foto}>IMG</span>}
+                    </div>
+                    <div className={styles.bloque_superior_derecho}>
+                        <div>
+                            <div className={styles.especie}>{producto.especie}</div>
+                            {producto.variedad !== "-" &&
+                                <div className={styles.variedad}>{producto.variedad}</div>
                             }
                         </div>
-                        <div className={styles.informacion}>
-                            <div className={styles.nombre}>
-                                {producto.especie}
-                                {" "}
-                                {producto.variedad !== "-" && <>{producto.variedad}</>}
-                            </div>
-                            <div className={styles.precio}>
-                                {producto.precio !== null ? <>$ {producto.precio}</> : "Sin precio"}
-                            </div>
+                        <div className={styles.precio}>
+                            {producto.precio ? <>${producto.precio}</> : "Sin precio"}
                         </div>
                     </div>
-                    <div className={styles.bloque_2}> 
-                        <div className={styles.etiquetas}>
-                            <span className={styles.etiqueta}>
-                                Presentacion {producto.presentacion}
-                            </span>
-                            <span className={styles.etiqueta}>
-                                Calibre {producto.calibre}
-                            </span>
-                            <span className={styles.etiqueta}>
-                                Categoria {producto.categoria}
-                            </span>
+                </div>
+                <div className={styles.bloque_medio}>
+                    <div className={styles.informacion_detallada}> 
+                        <div className={styles.nombre_informacion}>Presentacion</div>
+                        <div className={styles.valor_informacion}>
+                            <span className={styles.etiqueta}>{producto.presentacion}</span>
                         </div>
                     </div>
-                    <div className={styles.botones}>
-                        <span className={styles.botonPerfil}>
-                            Ver perfil
-                        </span>
-                        <span className={styles.botonWhatsApp}>
-                            <WhatsAppIcon sx={{ fontSize: "1.20rem" }} />
-                            {" "}
-                            WhatsApp
-                        </span>
+                    <div className={styles.informacion_detallada}> 
+                        <div className={styles.nombre_informacion}>Calibre</div>
+                        <div className={styles.valor_informacion}>
+                            <span className={styles.etiqueta}>{producto.calibre}</span>
+                        </div>
                     </div>
+                    <div className={styles.informacion_detallada}> 
+                        <div className={styles.nombre_informacion}>Categoria</div>
+                        <div className={styles.valor_informacion}>
+                            <span className={styles.etiqueta}>{producto.categoria}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.botones}>
+                    <button className={styles.botonWhatsApp}><WhatsAppIcon sx={{fontSize: "1.20rem"}}/> {" "} WhatsApp</button>
+                </div>
+                <div className={styles.bloque_inferior}>
+                    <span>Publicado por</span>
+                    <span className={styles.nombre_operador}>{producto.operador.nombreFantasia}</span>
                 </div>
             </div>
         </SwipeableDrawer>
